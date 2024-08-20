2b2be46af1fcc328e3bdbd89610e373b53f76bea
codenoid <14269809+codenoid@users.noreply.github.com>
2024-08-20 10:06:51 +0700

    implement basic auth, cidr, header modification

--- src/ServeLokalCommand.php
+++ src/ServeLokalCommand.php
@@ -5,6 +5,7 @@ namespace LokalSo\ServeLokal;
 use Illuminate\Foundation\Console\ServeCommand;
 use Symfony\Component\Console\Input\InputOption;
 use LokalSo\Lokal;
+use LokalSo\Options;
 
 class ServeLokalCommand extends ServeCommand
 {
@@ -90,17 +91,73 @@ class ServeLokalCommand extends ServeCommand
         $lanAddress = $this->input->getOption('lan-address') ?? "";
         $publicAddress = $this->input->getOption('public-address') ?? "";
         $tunnelName = $this->input->getOption('tunnel-name') ?? "";
+        $cidrAllow = $this->input->getOption('cidr-allow') ?? [];
+        $cidrDeny = $this->input->getOption('cidr-deny') ?? [];
+        $removeRequestHeaders = $this->input->getOption('remove-request-header') ?? [];
+        $removeResponseHeaders = $this->input->getOption('remove-response-header') ?? [];
+        $addRequestHeaders = $this->input->getOption('add-request-header') ?? [];
+        $addResponseHeaders = $this->input->getOption('add-response-header') ?? [];
+        $basicAuth = $this->input->getOption('basic-auth') ?? [];
+        $headerKeys = $this->input->getOption('header-key') ?? [];
 
-        $lokal  = new Lokal();
-        $lokal->newTunnel()
+        $lokal = new Lokal();
+        $tunnel = $lokal->newTunnel()
             ->setName($tunnelName)
             ->setTunnelType(Lokal::TunnelTypeHTTP)
             ->setLANAddress($lanAddress)
             ->setPublicAddress($publicAddress)
             ->setLocalAddress("{$this->host()}:{$this->port()}")
             ->showStartupBanner()
-            ->ignoreDuplicate()
-            ->create();
+            ->ignoreDuplicate();
+
+        $options = new Options();
+
+        // Apply CIDR allow rules
+        foreach ($cidrAllow as $cidr) {
+            $options->setCIDRAllow($cidr);
+        }
+
+        // Apply CIDR deny rules
+        foreach ($cidrDeny as $cidr) {
+            $options->setCIDRDeny($cidr);
+        }
+
+        // Remove request headers
+        foreach ($removeRequestHeaders as $header) {
+            $options->removeRequestHeader($header);
+        }
+
+        // Remove response headers
+        foreach ($removeResponseHeaders as $header) {
+            $options->removeResponseHeader($header);
+        }
+
+        // Add request headers
+        foreach ($addRequestHeaders as $header) {
+            list($key, $value) = explode(':', $header, 2);
+            $options->addRequestHeader(trim($key), trim($value));
+        }
+
+        // Add response headers
+        foreach ($addResponseHeaders as $header) {
+            list($key, $value) = explode(':', $header, 2);
+            $options->addResponseHeader(trim($key), trim($value));
+        }
+
+        // Set basic auth
+        foreach ($basicAuth as $auth) {
+            list($username, $password) = explode(':', $auth, 2);
+            $options->setBasicAuth(trim($username), trim($password));
+        }
+
+        // Set header keys
+        foreach ($headerKeys as $headerKey) {
+            list($key, $value) = explode(':', $headerKey, 2);
+            $options->setHeaderKey(trim($key), trim($value));
+        }
+
+        $tunnel->setOptions($options);
+        $tunnel->create();
 
         $this->components->info("Server running on [http://{$this->host()}:{$this->port()}]");
         $this->comment('  <fg=yellow;options=bold>Press Ctrl+C to stop the server</>');
@@ -118,6 +175,14 @@ class ServeLokalCommand extends ServeCommand
             ['lan-address', null, InputOption::VALUE_OPTIONAL, 'The LAN address to serve the application on', ''],
             ['public-address', null, InputOption::VALUE_OPTIONAL, 'The public address to serve the application on', ''],
             ['tunnel-name', null, InputOption::VALUE_OPTIONAL, 'The name of the tunnel being used', ''],
+            ['cidr-allow', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'CIDR ranges to allow', []],
+            ['cidr-deny', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'CIDR ranges to deny', []],
+            ['remove-request-header', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'Request headers to remove', []],
+            ['remove-response-header', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'Response headers to remove', []],
+            ['add-request-header', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'Request headers to add (format: "Key: Value")', []],
+            ['add-response-header', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'Response headers to add (format: "Key: Value")', []],
+            ['basic-auth', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'Basic auth credentials (format: "username:password")', []],
+            ['header-key', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'Header keys to set (format: "Key: Value")', []],
         ]);
     }
 }
